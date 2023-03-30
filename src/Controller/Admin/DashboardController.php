29285dc53d16fc0081c1d1e3363dba92bf4a70a9
HMH <musicbyhmh@gmail.com>
2023-03-30 12:43:01 +0200

    création de l'admin crud user

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -26,6 +26,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Liste des utilisateurs', 'fas fa-list', User::class);
     }
 }
